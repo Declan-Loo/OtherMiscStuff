1ae9ee788e28adb3d7d809ac3597c45f7cd49730
Declan Loo <188declan@gmail.com>
2023-01-15 18:53:52 +0800

    Create intro.py

---
+++ WebScraping/Intro-Wikipedia/intro.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.wikipedia.org/'
+r = requests.get(url)
+html_doc = r.text
+#Creates a soup object
+soup = BeautifulSoup(html_doc)
+
+#Get Text
+print(soup.get_text())
+
+#Get Prettify version of Soup
+print(soup.prettify())
+
+#Get Title
+print(soup.title)
+
+# Find all 'a' tags (which define hyperlinks) and Print the URLs to the shell
+for link in soup.find_all('a'):
+    print(link.get('href'))
